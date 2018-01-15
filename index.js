4fa22e2d580b72eee91195f823d5118876aa6aff
winter mutt <sejongsan@hotmail.com>
2018-01-15 18:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -122,19 +122,21 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
-  // implement me!
-  /**
-   * This function should move DODGER to the left
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 4}px`
+  }
 }
 
 function moveDodgerRight() {
-  // implement me!
-  /**
-   * This function should move DODGER to the right
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+  var rightNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(rightNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left + 4}px`
+  }
 }
 
 /**
